bf4061440f625e972a6deb3640763615383b5128
yang3529369110 <1443261811@qq.com>
2019-03-14 18:39:57 +0800

    1

--- resources/views/chatRoom.blade.php
+++ resources/views/chatRoom.blade.php
@@ -1,8 +1,6 @@
 @extends('layouts.admin')
 
 @section('css')
-    <link rel="stylesheet" href="/css/jquery.mCustomScrollbar.min.css"/>
-    <link rel="stylesheet" href="/css/jquery.emoji.css"/>
     <style type="text/css">
         .comment-text {
             margin-left: 50px !important;
@@ -63,17 +61,45 @@
             margin: 0;
             margin-left: 5px
         }
-        textarea::-webkit-input-placeholder{
-            color:#e6e6e6;
+
+        textarea::-webkit-input-placeholder {
+            color: #e6e6e6;
         }
-        textarea::-moz-placeholder{   /* Mozilla Firefox 19+ */
-            color:#e6e6e6;
+
+        textarea::-moz-placeholder { /* Mozilla Firefox 19+ */
+            color: #e6e6e6;
         }
-        textarea:-moz-placeholder{    /* Mozilla Firefox 4 to 18 */
-            color:#e6e6e6;
+
+        textarea:-moz-placeholder { /* Mozilla Firefox 4 to 18 */
+            color: #e6e6e6;
         }
-        textarea:-ms-input-placeholder{  /* Internet Explorer 10-11 */
-            color:#e6e6e6;
+
+        textarea:-ms-input-placeholder { /* Internet Explorer 10-11 */
+            color: #e6e6e6;
+        }
+
+        .labFace {
+            cursor: pointer;
+        }
+
+        .direct-chat-msg .direct-chat-timestamp{
+            float:right;
+        }
+        .right .direct-chat-timestamp {
+            float:left !important;
+        }
+        .right .direct-chat-name {
+            float:right !important;
+        }
+        .direct-chat-text {
+            display: inline-block;
+            margin: 5px 0 0 5px;
+        }
+
+        .direct-chat-primary .right > .direct-chat-text {
+            margin-right: 5px;
+            text-align: right;
+            float: right;
         }
     </style>
 @endsection
@@ -175,13 +201,13 @@
 
                 </div>
                 <!-- /.box-body -->
-                <div class="box-footer editor" style="padding-top:0">
+                <div class="box-footer editor" style="margin: 0;padding-top: 0">
                     <div class="box-header">
-                        <div class="box-tools" style="position: absolute; left:0;">
+                        <div class="box-tools" style="position: absolute; left:0;margin: 0;">
                             <div id="show"></div>
                             <label for="inputImage" class="image-label">
-                                {{--<input type="file" onchange="sendImageHandler(this)" accept="image/jpg,image/jpeg,image/png" name="file" id="inputImage" class="hidden">--}}
-                                <input type="file" accept="image/jpg,image/jpeg,image/png" name="file" id="inputImage" class="hidden upload">
+                                <input type="file" accept="image/jpg,image/jpeg,image/png" name="file" id="inputImage"
+                                       class="hidden upload">
                                 <i class="fa fa-photo"></i>
                             </label>
 
@@ -267,123 +293,212 @@
 @section('js')
     <script type="text/javascript" src="/js/jquery-browser.js"></script>
     <script type="text/javascript" src="/js/jquery.qqFace.js"></script>
+    <script type="text/javascript" src="/js/chat.js"></script>
     <script>
-        $(function () {
-            window.token = '{{csrf_token()}}';
-            var chatRoomHeight, chatRecordHeight;
-            autoChatRoomHeight();
+        //当前用户相关信息
+        window.uid = '{{auth()->id()}}';
+        window.name = '{{auth()->user()->name}}';
+        window.avatar = '/img/icon03.png';
+        window.to_id = '';
+        window.to_name = '';
+        window.client_id = '';
+        window.token = '{{csrf_token()}}';
 
-            $('.send').click(sendTextHandler);
-            $('.editor').on('click', '.labFace', sendFaceHandler);
-            $('.upload').change(sendImageHandler);
-            $("#text_in").get(0).focus();
+        window.ws = new WebSocket("ws://" + "127.0.0.1:8282");
 
-            $(window, ".wrapper").resize(function () {
-                autoChatRoomHeight();
-            });
-
-            $('.face').qqFace({
-                id: 'facebox',
-                assign: 'text_in',
-                path: '/img/arclist/'	//表情存放的路径
-            });
-
-            function replace_em(str) {
-                str = str.replace(/</g, '<；');
-                str = str.replace(/>/g, '>；');
-                str = str.replace(/ /g, '<；br/>；');
-                str = str.replace(/[em_([0-9]*)]/g, '<img src="face/$1.gif" border="0" />');
-                return str;
+        ws.onopen = function (e) {
+            if (e.target.readyState === 1) {
+                console.log('连接成功' + e.target.readyState);
             }
+        };
 
-            function autoChatRoomHeight() {
-                var neg = $('.main-header').outerHeight() + $('.main-footer').outerHeight();
-                var window_height = $(window).height();
-                var sidebar_height = $(".sidebar").height();
-                if (window_height >= sidebar_height) {
-                    chatRoomHeight = window_height - neg - 80;
-                    $(".nav-tabs-custom, .widget-user-2, .direct-chat").css('min-height', chatRoomHeight);
-                } else {
-                    chatRoomHeight = sidebar_height - 80;
-                    $(".nav-tabs-custom, .widget-user-2, .direct-chat").css('min-height', chatRoomHeight);
-                }
+        //监听消息
+        ws.onmessage = function (e) {
+            var response = JSON.parse(e.data);
+            var data = response.data;
 
-                var editorHeight = $('.editor').height();
-                chatRecordHeight = chatRoomHeight - 60 - editorHeight;
-                $('.direct-chat-messages').css('min-height', chatRecordHeight);
-            }
-
-            function sendTextHandler() {
-                var text = $('#text_in').val();
-                var elem = makeMessageHtml(text, 'left');
-                $('.direct-chat-messages').append(elem);
+            //如果有新消息就追加到dom中展示出来
+            if (response.message_type === 'chatMessage') {
+                //构建消息标签然后插入dom中
+                var _html = makeMessageHtml(data.content, data.avatar, 'right');
+                $(".direct-chat-messages").append(_html);
+                //聊天框默认最底部
                 positionBottom();
-                console.log(text);
-            }
-
-            function sendFaceHandler() {
-                var faceText = $(this).attr('labFace');
-                var labFace = $(this).parent().html();
-                var elem = makeMessageHtml(labFace, 'left');
-                $('.direct-chat-messages').append(elem);
-                positionBottom();
-                console.log(faceText);
-                console.log(labFace);
-            }
-
-            //发送图片消息
-            function sendImageHandler(e) {
-                var image = e.target.files[0];
-                if (!image) {
-                    return;
-                }
-
-                var formData = new FormData();
-                formData.append('image', image);
-                formData.append('_token', token);
-
-                $.ajax({
-                    url: '/chatLog/upload',
-                    type: 'POST',
-                    cache: false,
-                    data: formData,
-                    processData: false,
-                    contentType: false
-
-                }).done(function (res) {
+                //将接收到的消息标记为已读
+                $.get('chatLog/haveRead/' + data.message_id).done(function (res) {
                     console.log(res);
-                    //构建消息标签然后插入dom中
-                    var image = '<img src="'+res.url+'" style="height: 100px; width: 100px">';
-                    var _html = makeMessageHtml(image, 'left');
-                    $(".direct-chat-messages").append(_html);
-                    positionBottom();
-                    //聊天框默认最底部
-//                    positionBottom();
-                    //保存消息
-//                    storeMessage(res.url, 2);
-
-                }).fail(function (res) {
-                    console.log(res.responseJSON.message);
-                });
+                })
             }
 
-            function positionBottom() {
-                $(document).ready(function () {
-                    $(".direct-chat-messages").scrollTop($(".direct-chat-messages")[0].scrollHeight);
-                });
+            //如果socket连接成功保存自己的client_id
+            if (response.message_type === 'connectSuccess') {
+                window.client_id = response.client_id;
             }
+        };
 
-            function makeMessageHtml(content, point) {
-                var _html = '';
-                _html += '<div class="direct-chat-msg"><div class="direct-chat-info clearfix">';
-                _html += '<span class="direct-chat-name pull-left">Alexander Pierce</span>';
-                _html += '<span class="direct-chat-timestamp pull-right">23 Jan 2:00 pm</span></div>';
-                _html += '<img class="direct-chat-img" src="/img/user1-128x128.jpg" alt="Message User Image">';
-                _html += '<div class="direct-chat-text">' + content + '</div></div>';
+        getCustomers();
+        $('.send').click(sendTextHandler);
+        $('.editor').on('click', '.labFace', sendFaceHandler);
+        $('.upload').change(sendImageHandler);
+        $('.box-comments').on('click', '.box-comment', intoChatRoom);
+        $("#text_in").get(0).focus();
 
-                return _html;
-            }
+        $('.face').qqFace({
+            id: 'facebox',
+            assign: 'text_in',
+            path: '/img/arclist/'	//表情存放的路径
         });
+
+        function replace_em(str) {
+            str = str.replace(/</g, '<；');
+            str = str.replace(/>/g, '>；');
+            str = str.replace(/ /g, '<；br/>；');
+            str = str.replace(/[em_([0-9]*)]/g, '<img src="face/$1.gif" border="0" />');
+            return str;
+        }
+
+        function getCustomers() {
+            //获取客户列表
+            $.get('/customer/lists').done(function (response) {
+                var _html = '';
+                $.each(response, function (key, item) {
+                    _html += '<div class="box-comment" data-uid = ' + item.uid + '>';
+                    _html += '<img class="img-circle img-sm" src="' + item.avatar + '" alt="User Image"><div class="comment-text">';
+                    _html += '<span class="username">' + item.name + '';
+                    _html += item.unread > 0 ? '<span class="pull-right badge bg-red">' + item.unread + '</span>' : '';
+                    _html += '</span>It is a long established fact</div></div>';
+                });
+
+                $('.box-comments').append(_html);
+            });
+        }
+
+        function intoChatRoom() {
+            to_id = $(this).attr('data-uid');
+            //初始化客服与用户的的连接
+            $.post('/server/joinGroup/' + client_id, {'group_id': to_id, '_token': token})
+                .done(function (res) {
+                    console.log(res);
+                });
+            console.log('into room');
+            showChatRecord(to_id);
+        }
+
+        /**
+         * 获取聊天记录
+         * @param uid 客户的uid
+         * @param from 请求来源于客服人员还是用户
+         */
+        function showChatRecord(uid) {
+            var param = {'uid': uid, 'from': 'kf', '_token': token};
+            $.post('/chatLog/get', param, function (response) {
+                var _html = '';
+                $('.direct-chat-messages').html('');
+                $.each(response, function (index, item) {
+                    //如果消息来源客户那么消息显示在聊天窗口右侧
+                    var point = item.from_id == uid ? 'right' : 'left';
+                    _html += makeMessageHtml(item.content, item.from_avatar, point);
+                });
+
+                $(".direct-chat-messages").append(_html);
+                //聊天框默认最底部
+                positionBottom();
+            })
+        }
+
+        function sendTextHandler() {
+            var text = $('#text_in').val();
+            var elem = makeMessageHtml(text, avatar, 'left');
+            $('.direct-chat-messages').append(elem);
+            positionBottom();
+            storeMessage(text, 1);
+        }
+
+        function sendFaceHandler() {
+            var faceText = $(this).attr('labFace');
+            var labFace = $(this).parent().html();
+            var elem = makeMessageHtml(labFace, avatar, 'left');
+            $('.direct-chat-messages').append(elem);
+            positionBottom();
+            storeMessage(faceText, 3);
+        }
+
+        //发送图片消息
+        function sendImageHandler(e) {
+            var image = e.target.files[0];
+            if (!image) {
+                return;
+            }
+
+            var formData = new FormData();
+            formData.append('image', image);
+            formData.append('_token', token);
+
+            $.ajax({
+                url: '/chatLog/upload',
+                type: 'POST',
+                cache: false,
+                data: formData,
+                processData: false,
+                contentType: false
+
+            }).done(function (res) {
+                console.log(res);
+                //构建消息标签然后插入dom中
+                var image = '<img src="' + res.url + '" style="height: 100px; width: 100px">';
+                var _html = makeMessageHtml(image, avatar, 'left');
+                $(".direct-chat-messages").append(_html);
+                positionBottom();
+                //保存消息
+                storeMessage(res.url, 2);
+
+            }).fail(function (res) {
+                console.log(res.responseJSON.message);
+            });
+        }
+
+
+        function makeMessageHtml(content, avatar, point) {
+            point = point === 'right' ? 'right' : '';
+            var _html = '';
+            _html += '<div class="direct-chat-msg ' + point + '"><div class="direct-chat-info clearfix">';
+            _html += '<span class="direct-chat-name ">Alexander Pierce</span>';
+            _html += '<span class="direct-chat-timestamp">23 Jan 2:00 pm</span></div>';
+            _html += '<img class="direct-chat-img" src="' + avatar + '" alt="Message User Image">';
+            _html += '<div class="direct-chat-text">' + content + '</div></div>';
+
+            return _html;
+        }
+
+        /**
+         * 存储消息类容
+         * @param content 消息的类容
+         * @param contentType 消息的类型 1是文字消息 2是图片消息 3是表情消息
+         */
+        function storeMessage(content, contentType) {
+            var data = {
+                'from_id': uid,
+                'from_name': name,
+                'from_avatar': avatar,
+                'to_id': to_id,
+                'to_name': to_name,
+                'content': content,
+                'content_type': contentType,
+                '_token': token
+            };
+
+            $.post('/server/send/' + client_id, data, function (res) {
+                var err = res ? '保存成功' : '保存失败';
+                console.log(err);
+            }).complete(function (res) {
+                if (res.status !== 200) {
+                    $.each(res.responseJSON.errors, function (key, value) {
+                        console.log(value[0]);
+                        return false;
+                    });
+                }
+            })
+        }
 
     </script>
 @endsection
